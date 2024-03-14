364f89add15e7e29ebc1e20a330af4ab8edf2d53
Peter John Manlapig <pamanlapig@up.edu.ph>
2024-03-14 21:19:46 +0800

    exercise 2 ito po pala

--- Exercise 2/Exercise2_Manlapig.py
+++ Exercise 2/Exercise2_Manlapig.py
@@ -15,43 +15,42 @@ while True:
     print()
     print('LINE NO.', counter)
     distance = input ("Distance:  ")
-    bobo_magtype = False
-    while not (bobo_magtype):
-        distance = input ("Distance:  ")
-    if bobo_magtype:
-        print("bobo mo magtype")
-        continue
-    if not bobo_magtype:
-        break
-    azimuth = float(input (("Azimuth from the South:   ")))
-
-    if "-" in azimuth: # in DMS
+    # bobo_magtype = False
+    # while not (bobo_magtype):
+    #     distance = input ("Distance:  ")
+    # if bobo_magtype:
+    #     print("bobo mo magtype")
+    #     continue
+    # if not bobo_magtype:
+    #     break
+    azimuth = (input (("Azimuth from the South:   ")))
+    if "-" in azimuth: 
         degrees, minutes, seconds = azimuth.split("-")
-        azimuth = (int(degrees) + (int(minutes)/60) + (float(seconds)/3600))
+        azimuth = (int(degrees)) + (int(minutes)/60) + (float(seconds)/3600)%360
     else:
-        azimuth = (float(azimuth))
-
+        azimuth = float (azimuth)%360
+# assigning azimuth to bearing
     if azimuth > 0 and azimuth < 90:
-        bearing ="S  {: ^10} W" .format(azimuth)
+        bearing = 'S {: ^10} W' .format (azimuth)
     elif azimuth > 90 and azimuth < 180:
-        bearing = "N  {: ^10} W" .format(180 - azimuth)
+        bearing = 'N {: ^10} W' .format (180 - azimuth)
     elif azimuth > 180 and azimuth < 270:
-        bearing = "N  {: ^10} E" .format(azimuth - 180)
+        bearing = 'N {: ^10} E' .format (azimuth - 180)
     elif azimuth > 270 and azimuth < 360:
-        bearing = "N  {: ^10} E" .format(360 - azimuth)
-    elif azimuth == 0:
-        bearing = "Due South"
+        bearing = 'S {: ^10} E' .format (360 - azimuth)
+    elif azimuth == 00:
+        bearing = "DUE SOUTH"
     elif azimuth == 90:
-        bearing = "Due West"
+        bearing = "DUE WEST"
     elif azimuth == 180:
-        bearing= "Due North"
+        bearing = "DUE NORTH"
     elif azimuth == 270:
-        bearing = "Due East"
+        bearing = "DUE EAST"
     else:
-        "Nasaan ka"
+        bearing = "UNKNOWN"
 
 # create tuple of the line 
-    line = (counter, distance, azimuth)
+    line = (counter, distance, bearing)
     lines.append(line)
 
 # ASK FOR INPUT
@@ -65,11 +64,8 @@ while True:
 print("\n\n") 
 print('{: ^10} {: ^10} {: ^10}' .format ("LINE NO.", "DISTANCE", "BEARING"))
 for line in lines:
-    print( '{}{}{}'.format(line[0], line[1], line[2]))
+    print( '{: ^10} {: ^10} {: ^10}'.format(line[0], line[1], line[2]))
 
-
-print("LINES")
-print(lines)
 print("-----END-----")
 
 
