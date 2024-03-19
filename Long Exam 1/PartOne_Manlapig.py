9251c4e22a519c811c65d99b77564d7010dd077e
Peter John Manlapig <pamanlapig@up.edu.ph>
2024-03-19 18:03:09 +0800

    Manlapig - Exam Starts Here

---
+++ Long Exam 1/PartOne_Manlapig.py
@@ -0,0 +1,48 @@
+"""
+1. Procedural programming ay about procedures? siguro based sa name, may sinusunod na procedures ito. 
+At yung functional programming naman ay sa functions? Like pure functions ang ginagawa sa funcitonal programming?
+
+2. Development in Technological Industries tends to be progressed in almost infinite number of ways. Walang isang paraan sa 
+pagunlad ng Teknolohiya. At dahil dito, napakalaking tulong ng Git sa komunidad ng mga developers. 
+Collaboration is not only a step but a stride into more productive improvement in the industry. 
+The rapid progress comes from the noobie friendly environment of Git in which those who are new in the field can have access to the reliable
+codes of the experts. At dahil sa ito ay libre, maraming pwedeng makagamit nito at marami rin ang pwedeng makatulong dito. Bayanihan baga sa coding.
+Through the help of experts and professionals, version updates are regularly maintained and regulated. Thus ensuring the progress
+is of the highest of quality. 
+
+3. While Loop is used when the number of times the loop is executed is not known. On the other hand, For Loop is used when the number of times 
+the loop is executed is known. Halimbawa, sa machine exercise ay ginamit ang while loop nung humihingi ng lines na mayroong distance at azimuth.
+Hanggat nagyeyes pa sa while loop, magdadagdag pa ng panibagong lines. Humihinto lang ito kapag nasatisfy yung while not (False) at gagana yung loop breaks.
+So, pwede magdadagdag ng infinite number of times sa while loop, hanggat True ito. Sa For Loop naman, ginamit yung code na For line in lines.
+Kung saan ay inilalabas nya lang yung saktong bilang kung ilan yung nasa line. 
+
+4. The Divide and Conquer paradigm in programming is the most efficient way of solving problems, especially big ones. 
+In order for us not to be overwhelmed by how big the problem is, we can break it down into smaller and manageable ones.
+Halimbawa, ang problema ng traffic. Isa itong pambansang problema, bagkus ay isa itong malaking problema. 
+Pero kung gagamitin natin ang divide and conquer method, we can somehow alleviate the problem in a localized level. 
+In terms of programming, there are problems that require exhaustive debugging but can only be solved if it is done by stages.
+Gaya ng halimbawa natin, pwede muna nating hatiin sa dalawa yung malaking problema. Ang una ay ang problema sa dami ng sasakyan. 
+Ikalawa ay ang problema ng mga kalsada. In coding, once we have identified two smaller problems, which in reality
+are still somewhat big problems, we can further break it down into much smaller ones. 
+Kumbaga, hatiin pa natin sa dalawa ulit ang problema ng dami sasakyan. Sa mga pribadong sasakyan at sa mga pampublikong sasakyan. 
+Ganun din hatiin din natin sa dalawa ang problema ng mga kalsada. Sa mga masisikip na kalsada at sa kakulangan mismo ng kalsada. 
+Ngayon, ang problema sa traffic ay nahati na natin sa apat na mas maliliit na problema. 
+We can now try to provide solutions to these smaller problems that may and should affect the initial problem we have encountered.
+Ang isa sa mga nakita nating solusyon sa mga pribadong sasakyan ay ang pagpapatupad ng coding scheme kung saan naglilimita ng mga pribadong
+sasakyan sa lansangan. Ganun din, sa mga pampublikong sasakyan naman ay ang modernisasyon ng mga ito sa tulong ng gobyerno. 
+Sa kabilang dako naman, ang solusyon sa masisikip na kalsada ay ang road widening projects. Ang proyekto naman ng Build, BUild, Build ang 
+nakitang solusyon sa kakulangan ng mga kalsada. 
+Just like the traffic problem, problems and errors in coding are solved by systematically fixing the four smaller problems, thereby giving solutions to the 2 bigger ones
+and finally providing a way to solve the first and biggest problem.
+
+5. I think most of the tasks in the field of geomatics are already optimized programming-wise. But since students palang kami we have to do it
+manually. Hindi ko pa sure kung pinag isa na ba ang meaning ng Geodetic at Geomatics pero ang tinutukoy ko ay geodetic dahil nga puro general
+surveying pa naman yung ginagawa namin. One of the things we want to optimize is the computation part. Like the machine exercises done in this course,
+we can make programs that can take care of computations accurrately. Anything that can be computed systematically with proper equations can be 
+programmed.
+
+However, I think one example of a task related to geomatics is training and education. Just like the learning systems of Khan Academy, Duolingo,
+and many others, we can optimize learning geomatics through games, quizzes, and puzzles. Coding and CAD can be fun and enjoyable 
+if there are reward systems like in games. 
+
+"""
---
+++ Long Exam 1/PartTwo_Manlapig.py
@@ -0,0 +1,76 @@
+'''
+Print a title for your program. Include your name and a short description of your program.
+'''
+
+print("DIRECT LEVEL COMPUTER")
+print("Peter John Manlapig")
+print("This program solves direct levelling computations.")
+
+import math
+
+def floatInput():
+    '''
+    Input function
+
+    Input
+    BS reading 
+    FS reading 
+    HI
+    Elev
+
+    Output
+    float data type
+    '''
+
+    backsight = elev_turn_point + height_of_instrument
+
+    return backsight
+
+BS_Reading = floatInput(backsight)
+FS_Reading = floatInput(foresight)
+height_of_instrument = backsight - elev_turn_point
+# print(BS_Reading)
+
+'''
+Idedefine yung mga variables na gagamitin for leveling computation
+'''
+# backsight = ()
+# foresight = ()
+# height_of_instrument = ()
+# elev_turn_point = ()
+# levelling_table = [(backsight), (foresight), (height_of_instrument), (elev_turn_point)]
+
+tp_counter = 1
+lines = []
+total_distance = 0
+height_of_instrument = 0
+
+while True: 
+    print()
+    print("TP No. ", tp_counter)
+
+    distance = input("Distance: ")
+    azimuth = input("Azimuth from South: ")
+
+    elev_turn_point = floatInput()
+    backsight = floatInput(=float())
+    foresight = floatInput(=float())
+
+    line = (tp_counter, BS_Reading, height_of_insrument, FS_Reading, elev_turn_point)
+    lines.append(line)
+
+    yn = input("Add New Turning Point? ")
+    if yn.lower() == "yes" or yn.lower()  == "y":
+        tp_counter = tp_counter + 1
+        continue
+    else:
+        break
+
+print("\n")
+'''
+Ipiprint yung guide table
+'''
+print('{: ^10} {: ^15} {: ^25} {: ^15} {: ^15}' .format ("STATION", "BACKSIGHT", "INSRUMENT HEIGHT", "FORESIGHT", "ELEVAION"))
+for line in lines:
+    print( '{:^10} {: ^15} {: ^25} {: ^15} {: ^15}'.format(line[0], line[1], line[2], line[3], line[4]))
+
