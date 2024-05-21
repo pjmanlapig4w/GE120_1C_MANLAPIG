b8ce9d6d0a71448458bf88d566663af834394061
pjmanlapig4w <pamanlapig@up.edu.ph>
2024-05-21 17:30:30 +0800

    essay

---
+++ Long Exam 2/PartOne_Manlapig.py
@@ -0,0 +1,27 @@
+"""
+1. Procedural Programming ay ginagamit sa simpler tasks at more straightforward workflows 
+while Object-Oriented Programming naman can be used for more complicated systems. Procedural programming
+follows procedures like sunod sunod lang sya, following a sequence of actions but in OOP, many objects 
+interact with one another yung parang modules na may sari-sariling attributes and methods. 
+
+2. GUI also known as Graphical User Interface brings the programs to the users. Dito pwedeng mag interact 
+ang mga users sa mga digital programs. Since ito ay graphical, ang usual elements nito ay visual at nakadepende 
+sa designs na nakikita ng users. At dahil we are focusing on geomatics, like maps and measurements, GUI provides 
+a much larger scale of opportunity para maging accessible ang products and processes na ito to more people. 
+
+3. Operation Overloading in OOP means the increase of usability of an operator. Usually, integers lang ang 
+ginagamitan ng operation like ng addition and subtraction. But because of Operation Overloading, pwede narin gamitin
+ang operations sa string variables. 
+
+4. Inheritance in OOP means pinapayagan or ina allow yung bagong class na gamitin yung attributes or methods ng naunang 
+class. Ito yung pinaka fundamental aspect ng Object-Oriented Programming. Dahil dito, mas naipopromote yung reusability
+ng mga codes at mas madaling imaintain yung mga classes ng mga codes since magkakaron ng hierarchy of classes. 
+
+5. In Full Stack Development, the front-end is the part that the users interact with. Ito yung nakikita ng mga end-users.
+Dito lumalabas ang creativity ng mga programmers dahil graphics ang unang mapapansin dito. Ang isa pa sa basehan ng 
+magandang front-end ay yung ease of use nito. Kapag hindi na need ipaliwanag, masasabing user friendly ito at mas
+tatangkilikin ito. Habang yung back-end naman ay ang part na nagpapagana sa front-end. If there are call to action sa 
+front-end, sa back-end gumagana ang mga operations nito. Ito yung nagsisilbing backbone ng program or application.
+Lastly, ang database naman ang nagsisilbing storage system ng program or application. Dito hinihugot yung mga files 
+na kinakailangan ipublish sa front-end or yung mga variables na need sa operation sa back-end. 
+"""
